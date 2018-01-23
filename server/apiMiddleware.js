bca8ec81d2e22250d37d6b0abac2e830084b90fb
Jonas Barsten <jonasbarsten@gmail.com>
2018-01-23 20:11:48 +0100

    Adding getTracksByReleaseId API-endpoint

--- server/apiMiddleware.js
+++ server/apiMiddleware.js
@@ -3,66 +3,6 @@ import ConnectRoute from 'connect-route';
 
 let whiteList = Meteor.settings.private.apiWhitelist;
 
-function getArtistById (req, res, next) {
-	const origin = req.headers.origin;
-	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
-
-	if (allow) {
-		let artist = Artists.findOne({_id: req.params.id});
-		if (artist) {
-			artist = JSON.stringify(artist);
-			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end(artist);
-		} else {
-			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end();
-		}
-	} else {
-		res.writeHead(403);
-		res.end();
-	}
-}
-
-function getArtistByName (req, res, next) {
-	const origin = req.headers.origin;
-	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
-
-	if (allow) {
-		let artist = Artists.findOne({name: req.params.name});
-		if (artist) {
-			artist = JSON.stringify(artist);
-			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end(artist);
-		} else {
-			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end();
-		}
-	} else {
-		res.writeHead(403);
-		res.end();
-	}
-}
-
-function getReleasesByArtistId (req, res, next) {
-	const origin = req.headers.origin;
-	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
-
-	if (allow) {
-		let releases = Releases.find({artists: {$in: [req.params.id]}}).fetch();
-		if (releases) {
-			releases = JSON.stringify(releases);
-			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end(releases);
-		} else {
-			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
-			res.end();
-		}
-	} else {
-		res.writeHead(403);
-		res.end();
-	}
-}
-
 function listArtists (req, res, next) {
 	const origin = req.headers.origin;
 	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
@@ -125,13 +65,94 @@ function listTracks (req, res, next) {
 	}
 }
 
+function getArtistById (req, res, next) {
+	const origin = req.headers.origin;
+	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
+
+	if (allow) {
+		let artist = Artists.findOne({_id: req.params.id});
+		if (artist) {
+			artist = JSON.stringify(artist);
+			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end(artist);
+		} else {
+			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end();
+		}
+	} else {
+		res.writeHead(403);
+		res.end();
+	}
+}
+
+function getArtistByName (req, res, next) {
+	const origin = req.headers.origin;
+	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
+
+	if (allow) {
+		let artist = Artists.findOne({name: req.params.name});
+		if (artist) {
+			artist = JSON.stringify(artist);
+			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end(artist);
+		} else {
+			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end();
+		}
+	} else {
+		res.writeHead(403);
+		res.end();
+	}
+}
+
+function getReleasesByArtistId (req, res, next) {
+	const origin = req.headers.origin;
+	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
+
+	if (allow) {
+		let releases = Releases.find({artists: {$in: [req.params.id]}}).fetch();
+		if (releases) {
+			releases = JSON.stringify(releases);
+			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end(releases);
+		} else {
+			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end();
+		}
+	} else {
+		res.writeHead(403);
+		res.end();
+	}
+}
+
+function getTracksByReleaseId (req, res, next) {
+	const origin = req.headers.origin;
+	const allow = (whiteList.indexOf(origin) != -1) ? true : false;
+
+	if (allow) {
+		let tracks = Tracks.find({release: req.params.id}).fetch();
+		if (tracks) {
+			tracks = JSON.stringify(tracks);
+			res.writeHead(200, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end(tracks);
+		} else {
+			res.writeHead(404, {'Access-Control-Allow-Origin': origin, 'Access-Control-Allow-Methods': 'GET'});
+			res.end();
+		}
+	} else {
+		res.writeHead(403);
+		res.end();
+	}
+}
+
 const middleware = ConnectRoute( function(router) {
-	router.get('/api/artistById/:id', getArtistById);
-	router.get('/api/artistByName/:name', getArtistByName);
-	router.get('/api/releasesByArtistId/:id', getReleasesByArtistId);
 	router.get('/api/artists', listArtists);
 	router.get('/api/releases', listReleases);
 	router.get('/api/tracks', listTracks);
+	router.get('/api/artistById/:id', getArtistById);
+	router.get('/api/artistByName/:name', getArtistByName);
+	router.get('/api/releasesByArtistId/:id', getReleasesByArtistId);
+	router.get('/api/tracksByReleaseId/:id', getTracksByReleaseId);
 });
 
 WebApp.connectHandlers.use(middleware);
